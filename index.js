5c1282ad2af7b1227a2ef3b7812289e72c5cda46
ErickFPCruz <erick.cruz97@hotmail.com>
2022-11-16 20:39:53 -0300

    Estou no caminho... só tem um porém

---
+++ index.js
@@ -0,0 +1,14 @@
+const nrPrincipal = ` `
+
+prompt = (`Escreva um número qualquer: ${nrPrincipal}`);
+
+let contador = -1;
+
+alert(`A tabuada do número ${nrPrincipal} é: `);
+
+while (contador++ < 10)
+{    
+    let produto = nrPrincipal * contador;
+    `${nrPrincipal} x ${contador} = ${produto}`;
+}
+return;
